88a3166f49eeea6ef0d59cfa03bb9d7ccb8b2f29
☆ θ V!RMSKVLL  Δ ☆ <44964856+VIRMSKVLL@users.noreply.github.com>
2025-12-05 20:17:49 -0800

    I could hardcode this but i wanted to try adding it to the config

--- src/config.cpp
+++ src/config.cpp
@@ -170,6 +170,7 @@ void Config::read(int argc, char *argv[]) {
         {"integerScalingLastMile", true},
         {"maxTextureSize", 0},
         {"gameFolder", ""},
+        {"rgss_scripts", "xScripts.rvdata2"},
         {"anyAltToggleFS", false},
         {"enableReset", true},
         {"enableSettings", true},
@@ -239,6 +240,7 @@ try { exp } catch (...) {}
 #define SET_STRINGOPT(var, key) GUARD(var = std::string(opts[#key].as_string());)
     
     SET_STRINGOPT(gameFolder, gameFolder);
+    SET_STRINGOPT(game.scripts, rgss_scripts);
     SET_STRINGOPT(dataPathOrg, dataPathOrg);
     SET_STRINGOPT(dataPathApp, dataPathApp);
     SET_STRINGOPT(iconPath, iconPath);
